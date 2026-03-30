76836903bc4834b9c3c9210400f5fb54f7c14b74
Lokeshsolanki12 <lokesh.solanki7014@gmail.com>
2026-03-30 17:33:13 +0000

    Initial shell scripting files

---
+++ 02_commant.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Script with lokesh solanki "
+# But this is startting of learning Bash 
+
+
+
+<<comment
+
+This is 
+Multilien comments
+comment
---
+++ 04_constant_var.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Script to show how to use veriable
+
+a=10
+name="Lokesh"
+age=19
+
+echo "My name is $name and my age is $age . "
+
+name="Lucky"
+
+echo "My name is $name . "
+
+
+# Var to store the output of command
+
+
+HSOTNAME=$(hostname)
+
+echo " Name of this machine is $HOSTNAME"
+
+readonly COLLEGE="Arya"
+
+echo "My college name is $COLLEGE"
+
+
+
